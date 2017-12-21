6d882c6c6b039696304ecd2872b48dfb7b2f58e0
Tri hotboy <tridoan1998@gmail.com>
2017-12-21 15:13:31 -0800

    Python codes for small programs
    small projects to practice Python

---
+++ Practice_Python/HangmanGame.py
@@ -0,0 +1,23 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("crimson")        # set the window background color
+
+tess = turtle.Turtle()
+tess.color("blue")              # make tess blue
+tess.pensize(3)                 # set the width of her pen
+
+tess.forward(50)
+tess.left(120)
+tess.forward(50)
+
+wn.exitonclick()                # wait for a user click on the canvas
+
+
+space1 = ""
+space2 = ""
+space3 = ""
+space4 = ""
+
+print(space1, space2, space3, space4)
+print("_", "_", "_", "_")
