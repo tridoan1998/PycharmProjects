e7b94df2d67df4740dea921d75a1fad3090999c8
Tri hotboy <tridoan1998@gmail.com>
2017-12-23 14:22:05 -0800

    updated

--- Practice_Python/HangmanGame.py
+++ Practice_Python/HangmanGame.py
@@ -5,19 +5,12 @@ BG.bgcolor("crimson")
 
 tess = turtle.Turtle()
 tess.color("blue")              # make tess blue
-tess.pensize(3)                 # set the width of her pen
+tess.pensize(4)                 # set the width of her pen
+
+turtle.write("Hello", move=False, align="left", font=("Arial", 8, "normal"))
 
 tess.forward(50)
-tess.left(120)
-tess.forward(50)
+#tess.left(120)
 
-wn.exitonclick()                # wait for a user click on the canvas
+BG.exitonclick()                # wait for a user click on the canvas
 
-
-space1 = ""
-space2 = ""
-space3 = ""
-space4 = "dfsdff"
-
-print(space1, space2, space3, space4)
-print("_", "_", "_", "_")
