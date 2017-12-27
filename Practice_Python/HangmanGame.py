373dd46fb296946bd894249d1333c7c914b7be51
Tri hotboy <tridoan1998@gmail.com>
2017-12-27 00:33:20 -0800

    new codes

--- Practice_Python/HangmanGame.py
+++ Practice_Python/HangmanGame.py
@@ -3,14 +3,23 @@ import turtle
 #class MyTurtle(turtle.Turtle):
  #   def __init__(self, )
 BG = turtle.Screen()
-BG.bgcolor("crimson")
+BG.bgcolor("lightgreen")
+
+BG.title("Hangnam Game")
 
 
-turtle.write("Hello", move=False, align="left", font=("Arial", 8, "normal"))
+turtle.write("Hello", move=False, align="right", font=("Time New Roman", 15, "normal"))
 
-turtle.circle(40)
-turtle.down()
-turtle.right(90)
-turtle.forward(30)
+#BG.delay(50)
+
+frame = turtle.Turtle()
+frame.left(90)
+frame.forward(270)
+frame.right(90)
+frame.forward(180)
+frame.right(90)
+frame.forward(70)
+frame.right(90)
+frame.circle(40 )
 BG.exitonclick()                # wait for a user click on the canvas
 
