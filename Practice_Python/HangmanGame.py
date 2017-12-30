45edc2a444e05ca5fdf89139a67797e5b2625152
Tri hotboy <tridoan1998@gmail.com>
2017-12-29 20:52:58 -0800

    finished drawing the frame

--- Practice_Python/HangmanGame.py
+++ Practice_Python/HangmanGame.py
@@ -1,18 +1,17 @@
 import turtle
 
-#class MyTurtle(turtle.Turtle):
- #   def __init__(self, )
 BG = turtle.Screen()
 BG.bgcolor("lightgreen")
-
 BG.title("Hangnam Game")
 
-
-turtle.write("Hello", move=False, align="right", font=("Time New Roman", 15, "normal"))
-
-#BG.delay(50)
-
 frame = turtle.Turtle()
+frame.hideturtle()
+frame.color("blue", "red")
+frame.pensize(2)
+frame.penup()
+frame.goto(-300,-100)
+frame.pendown()
+frame.begin_fill()
 frame.left(90)
 frame.forward(270)
 frame.right(90)
@@ -20,6 +19,13 @@ frame.forward(180)
 frame.right(90)
 frame.forward(70)
 frame.right(90)
-frame.circle(40 )
+frame.circle(40)
+
+turtle.write("Hello", move=False, align="right", font=("Time New Roman", 15, "normal"))
+
+#def frame_draw(frame):
+
+
+
 BG.exitonclick()                # wait for a user click on the canvas
 
