ece71ed384bb07f4221f189d6cb586b3269be74a
Tri hotboy <tridoan1998@gmail.com>
2017-12-31 02:33:34 -0800

    new

--- Practice_Python/HangmanGame.py
+++ Practice_Python/HangmanGame.py
@@ -1,31 +1,55 @@
 import turtle
-
 BG = turtle.Screen()
 BG.bgcolor("lightgreen")
 BG.title("Hangnam Game")
 
-frame = turtle.Turtle()
-frame.hideturtle()
-frame.color("blue", "red")
-frame.pensize(2)
-frame.penup()
-frame.goto(-300,-100)
-frame.pendown()
-frame.begin_fill()
-frame.left(90)
-frame.forward(270)
-frame.right(90)
-frame.forward(180)
-frame.right(90)
-frame.forward(70)
-frame.right(90)
-frame.circle(40)
 
-turtle.write("Hello", move=False, align="right", font=("Time New Roman", 15, "normal"))
-
-#def frame_draw(frame):
+def draw_frame():
+    frame = turtle.Turtle()
+    frame.hideturtle()
+    frame.color("blue", "red")
+    frame.pensize(2)
+    frame.penup()
+    frame.goto(-300,-100)
+    frame.pendown()
+    frame.begin_fill()
+    frame.left(90)
+    frame.forward(270)
+    frame.right(90)
+    frame.forward(180)
+    frame.right(90)
+    frame.forward(70)
+    frame.right(90)
+    frame.circle(35)
 
 
+def question_format():
+    word = turtle.Turtle()
+    word.hideturtle()
+    word.penup()
+    word.goto(200, 220)
+    word.color("blue")
+    word.write("Question: Who is Tri currently have a crush on right now?", move=False, align="right", font=("Time New Roman", 15, "normal"))
 
-BG.exitonclick()                # wait for a user click on the canvas
+
+def first_spot():
+    first = turtle.Turtle()
+
+
+def text_input():
+    x = ""
+    turtle.textinput("Letter go here: ", x)
+    if(x == "N"):
+
+    else if (x ==  "G")
+
+    else if(x == "A")
+
+draw_frame()
+question_format()
+BG.mainloop()
+
+
+
+
 
