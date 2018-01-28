e4e7730eac276e79ba058427b57ff79ffef08fbd
Tri hotboy <tridoan1998@gmail.com>
2018-01-28 00:28:40 -0800

    more codes

--- Practice_Python/Linked_List.py
+++ Practice_Python/Linked_List.py
@@ -29,9 +29,14 @@ class link_list():
             temp = Node(data)
             self.head = temp
             self.tail = temp
+        elif self.size is 1:
+            temp = Node(data)
+
+            self.head = temp
+            self.tail = temp
         else:
             temp = Node(data)
-            self.tail.next = temp
+            temp.next = self.tail
             self.tail = temp
         self.size += 1
 
@@ -56,7 +61,7 @@ class link_list():
             temp = self.head
             self.head = self.head.next
             del temp
-        self.size -= 1   
+        self.size -= 1
 
     def remove_stop(self):
         if self.size is 0:
@@ -82,15 +87,61 @@ class link_list():
             count += 1
             if temp.next is None:
                 return print("The list has no data with the value ", value)
-        return print(count)
+        return count
+
+    def print_data_at_current_node(self, position):
+        if self.size is 0:
+            print("The list is already empty!")
+            return
+        else:
+            temp = self.head
+            count = 1
+            while count < position -1:
+                temp = temp.next
+                count += 1
+            if self.size is 1:
+                del self.head
+                self.head = self.tail = None
+            else:
+
+                print(temp.data)
+    def delete_data_at_current_node(self, position):
+        temp = self.head
+        if temp is None:
+            print("The list is already empty!")
+            return
+        elif position > self.size:
+            print("The list is smaller than that.")
+            return
+        else:
+            count = 1
+            while count < (position - 1):
+                temp = temp.next
+                count += 1
+
+    def ask_for_position(self):
+        position = input("PLease enter the position you want to insert: ")
+        return position
+
+    def insert_at_current_node(self, data):
+        if self.head is None:
+            print("The list has no element so we will insert to the front.")
+            self.head = Node(data)
+            print(self.head.data)
 
 linklist = link_list()
-
+linklist.insert_at_current_node(2)
 linklist.insert_end(13)
 linklist.insert_end(20)
 linklist.insert_end(100)
 linklist.insert_end(40)
 
+linklist.print()
 print("The size of the list is:", linklist.print_size())
-linklist.search(20)
+print("Position of number 20 is:", linklist.search(20))
 
+linklist.print_data_at_current_node(2)
+print("The list now has: ")
+linklist.print()
+
+position = linklist.ask_for_position()
